b01941c932d6ce1bb2654bb52eb6f0f87e2b6822
Daiyan-hash <78990783+Daiyan-hash@users.noreply.github.com>
2021-07-02 17:46:27 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var background, backgroundImg;
+var runner1, runner2, runner3, runner4;
+var runner1Img, runner2img, runner3Img, runner4Img;
+
+function preload(){
+backgroundImg=loadImage("images/th.jpeg");
+runner1Img=loadImage("images/runner1.png");
+runner2Img=loadImage("images/runner2.png");
+runner3Img=loadImage("images/runner1.png");
+runner4Img=loadImage("images/runner2.png");
+}
+
+function setup() {
+  canvas = createCanvas(1000,800);
+ 
+}
+
+function draw(){
+  background(backgroundImg)
+ 
+ // drawSprites();
+}
